37e3bc683b1f0e3ba701527bae9a7801feb2920d
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-02-10 11:00:24 +0100

    Finished the menu section.

--- src/StudentUtilityApp.java
+++ src/StudentUtilityApp.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class StudentUtilityApp {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-
+        System.out.println("""
+                ==== STUDENT UTILITY APP ====
+                1. Add two numbers
+                2. Check is a number is EVEN or ODD
+                3. Print numbers from 1 to N
+                4. Display day of the week
+                5. Exit
+                """);
+        System.out.print("Enter your choice:");
+        int choice = scan.nextInt();
     }
 }
