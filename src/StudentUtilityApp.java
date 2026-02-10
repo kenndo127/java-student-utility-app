b7e3f39b04ee61c7a76c3571d15322a3024ec51a
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-02-10 11:38:46 +0100

    Implemented the print weekday method.

--- src/StudentUtilityApp.java
+++ src/StudentUtilityApp.java
@@ -28,6 +28,7 @@ public class StudentUtilityApp {
                 break;
             case(4):
                 // call the Display the day of the week function
+                dayOfTheWeek();
                 break;
             case(5):
                 System.out.println("Thank You for Using the app");
@@ -58,6 +59,36 @@ public class StudentUtilityApp {
 
         for(int i = 1; i <= num; i++){
             System.out.println(i);
+        }
+    }
+    public static void dayOfTheWeek(){
+        System.out.print("Enter a weekday number: ");
+        int weekdayNum = scan.nextInt();
+
+        switch(weekdayNum){
+            case(1):
+                System.out.println("Monday");
+                break;
+            case(2):
+                System.out.println("Tuesday");
+                break;
+            case(3):
+                System.out.println("Wednesday");
+                break;
+            case(4):
+                System.out.println("Thursday");
+                break;
+            case(5):
+                System.out.println("Friday");
+                break;
+            case(6):
+                System.out.println("Saturday");
+                break;
+            case(7):
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day.");
         }
     }
 }
